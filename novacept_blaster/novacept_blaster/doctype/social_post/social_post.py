bae8a99c263d9af070da87272ddf94df4953ae38
HarshMagiya7 <97886782+HarshMagiya7@users.noreply.github.com>
2022-11-07 17:55:37 +0530

    Adding whatsapp button

--- novacept_blaster/novacept_blaster/doctype/social_post/social_post.py
+++ novacept_blaster/novacept_blaster/doctype/social_post/social_post.py
@@ -11,62 +11,63 @@ from frappe import _
 from frappe.model.document import Document
 
 class SocialPost(Document):
-	def validate(self):
-		if not self.facebook and not self.instagram and not self.linkedin :
-			frappe.throw(_("Select atleast one Social Media Platform"))
+        def validate(self):
+                if not self.facebook and not self.instagram and not self.linkedin :
+                        frappe.throw(_("Select atleast one Social Media Platform"))
 
-		if self.scheduled_time:
-			current_time = frappe.utils.now_datetime() - datetime.timedelta(seconds = 120)
-			scheduled_time = frappe.utils.get_datetime(self.scheduled_time)
-			if scheduled_time < current_time:
-				frappe.throw(_("Scheduled Time must be a future time"))
+                if self.scheduled_time:
+                        current_time = frappe.utils.now_datetime() - datetime.timedelta(seconds = 120)
+                        scheduled_time = frappe.utils.get_datetime(self.scheduled_time)
+                        if scheduled_time < current_time:
+                                frappe.throw(_("Scheduled Time must be a future time"))
 
-	def submit(self):
-		if self.scheduled_time:
-			self.post_status = "Scheduled"
-		super(SocialPost, self).submit()
+        def submit(self):
+                if self.scheduled_time:
+                        self.post_status = "Scheduled"
+                super(SocialPost, self).submit()
 
-	def on_cancel(self):
-		self.db_set("post_status", "Cancelled")
+        def on_cancel(self):
+                self.db_set("post_status", "Cancelled")
 
-	@frappe.whitelist()
-	def post(self):
-		try:
-			if self.facebook:
-				facebook = frappe.get_doc("Facebook Setting",self.page_name)
-				if self.media_type:
-					media_url = frappe.utils.get_url() + self.media
-					facebook_post = facebook.post(self.text,self.page_name ,self.media_type ,media_url,self.fb_link)
-				else:
-#					facebook_post = facebook.post(self.text,self.page_name ,self.fb_link)
-					facebook_post = facebook.post(self.text,self.page_name,self.media_type,self.media ,self.fb_link)
-			if self.instagram:
-				media_url = frappe.utils.get_url() + self.media
-				instagram = frappe.get_doc("Instagram Setting",self.acc_name)
-				instagram_post = instagram.post(self.caption,self.acc_name, media_url,self.media_type)
-			if self.linkedin:
-				linkedin = frappe.get_doc("LinkedIn Setting",self.link_page_name)
-				linkedin_post = linkedin.post(self.linkedin_post, self.title, self.media)
-				print(f'\n\n\n\n{linkedin_post.headers["X-RestLi-Id"]}\n\n\n\n')
-			self.db_set("post_status", "Posted")
+        @frappe.whitelist()
+        def post(self):
+                try:
+                        if self.facebook:
+                                facebook = frappe.get_doc("Facebook Setting",self.page_name)
+                                if self.media_type:
+                                        media_url = frappe.utils.get_url() + self.media
+                                        print(f'\n\n\n\n{media_url}\n\n\n')
+                                        facebook_post = facebook.post(self.text,self.page_name ,self.media_type ,media_url,self.fb_link,self.whatsapp_button, self.whatsapp_button_label)
+                                else:
+#                                       facebook_post = facebook.post(self.text,self.page_name ,self.fb_link)
+                                        facebook_post = facebook.post(self.text,self.page_name,self.media_type,self.media ,self.fb_link,self.whatsapp_button, self.whatsapp_button_label)
+                        if self.instagram:
+                                media_url = frappe.utils.get_url() + self.media
+                                instagram = frappe.get_doc("Instagram Setting",self.acc_name)
+                                instagram_post = instagram.post(self.caption,self.acc_name, media_url,self.media_type)
+                        if self.linkedin:
+                                linkedin = frappe.get_doc("LinkedIn Setting",self.link_page_name)
+                                linkedin_post = linkedin.post(self.linkedin_post, self.title, self.media)
+                                print(f'\n\n\n\n{linkedin_post.headers["X-RestLi-Id"]}\n\n\n\n')
+                        self.db_set("post_status", "Posted")
 
-		except Exception as e:
-			frappe.throw(_(e))
-			self.db_set("post_status", "Error")
-			self.log_error("Social posting failed")
+                except Exception as e:
+                        frappe.throw(_(e))
+                        self.db_set("post_status", "Error")
+                        self.log_error("Social posting failed")
 
 def process_scheduled_social_media_posts():
-	print('\n\n\n\n\n\n\n PROCESS \n\n\n\n\n\n')
-	posts = frappe.get_list(
-		"Social Post",
-		filters={"post_status": "Scheduled", "docstatus": 1},
-		fields=["name", "scheduled_time"],
-	)
-	start = frappe.utils.now_datetime() - datetime.timedelta(minutes=10)
-	end = frappe.utils.now_datetime() + datetime.timedelta(minutes=10)
-	for post in posts:
-		if post.scheduled_time:
-			post_time = frappe.utils.get_datetime(post.scheduled_time)
-			if post_time > start and post_time <= end:
-				sm_post = frappe.get_doc("Social Post", post.name)
-				sm_post.post()
+        print('\n\n\n\n\n\n\n PROCESS \n\n\n\n\n\n')
+        posts = frappe.get_list(
+                "Social Post",
+                filters={"post_status": "Scheduled", "docstatus": 1},
+                fields=["name", "scheduled_time"],
+        )
+        start = frappe.utils.now_datetime() - datetime.timedelta(minutes=10)
+        end = frappe.utils.now_datetime() + datetime.timedelta(minutes=10)
+        for post in posts:
+                if post.scheduled_time:
+                        post_time = frappe.utils.get_datetime(post.scheduled_time)
+                        if post_time > start and post_time <= end:
+                                sm_post = frappe.get_doc("Social Post", post.name)
+                                sm_post.post()
